005943922b097353a3a85cfd506e4f16e4bc0b3f
Dave Williams <dave@dave.io>
2024-10-26 22:05:03 +0100

    add picard imperfect albums script and README

---
+++ README.md
@@ -0,0 +1,17 @@
+# Miscellaneous Scripts
+
+## `gandi_domain_search.py`
+
+Gets prices for domains currently registered on Gandi
+
+## `ghc.sh`
+
+GitHub helper to keep sources organised
+
+## `remove_imperfect_albums.py`
+
+MusicBrainz Picard plugin to remove imperfect (not yellow) albums from the match list
+
+## `splitme.sh`
+
+`chunkwm` utility script
---
+++ remove_imperfect_albums.py
@@ -0,0 +1,25 @@
+PLUGIN_NAME = "Remove Impefdrfect Albums"
+PLUGIN_AUTHOR = "daveio"
+PLUGIN_DESCRIPTION = """Remove all imperfectly matched albums from the selection."""
+PLUGIN_VERSION = "0.3"
+PLUGIN_API_VERSIONS = ["2.0", "2.1", "2.2", "2.3"]
+PLUGIN_LICENSE = "GPL-2.0"
+PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
+
+from PyQt5.QtCore import QCoreApplication
+
+from picard.album import Album
+from picard.ui.itemviews import BaseAction, register_album_action
+
+
+class RemoveImperfectAlbums(BaseAction):
+    NAME = "Remove imperfect albums"
+
+    def callback(self, objs):
+        for album in objs:
+            if isinstance(album, Album) and album.loaded and not album.is_complete():
+                self.tagger.remove_album(album)
+            QCoreApplication.processEvents()
+
+
+register_album_action(RemoveImperfectAlbums())
