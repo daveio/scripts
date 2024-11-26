7344f6855d257ed3b9f504f8ba6c39dcbac48379
Dave Williams <dave@dave.io>
2024-11-26 13:51:45 +0000

    addl media remove fix

--- mastodon-maintenance/maint.sh
+++ mastodon-maintenance/maint.sh
@@ -1,5 +1,5 @@
 RAILS_ENV=production bin/tootctl cache clear
 RAILS_ENV=production bin/tootctl media remove-orphans
-RAILS_ENV=production bin/tootctl media remove --days=7 --include-follows --verbose
+RAILS_ENV=production bin/tootctl media remove --days=7 --verbose
 RAILS_ENV=production bin/tootctl media remove --days=7 --prune-profiles --include-follows --verbose
 RAILS_ENV=production bin/tootctl media remove --days=7 --remove-headers --include-follows --verbose
