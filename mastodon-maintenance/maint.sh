8fd32519bd09c0219becd35d2c5c4a950f11f22f
Dave Williams <dave@dave.io>
2024-11-26 13:50:09 +0000

    verbose

--- mastodon-maintenance/maint.sh
+++ mastodon-maintenance/maint.sh
@@ -1,4 +1,4 @@
 RAILS_ENV=production bin/tootctl cache clear
 RAILS_ENV=production bin/tootctl media remove-orphans
-RAILS_ENV=production bin/tootctl media remove --days=7 --prune-profiles --remove-headers --include-follows
-RAILS_ENV=production bin/tootctl media remove --days=7 --remove-headers --include-follows
+RAILS_ENV=production bin/tootctl media remove --days=7 --prune-profiles --include-follows --verbose
+RAILS_ENV=production bin/tootctl media remove --days=7 --remove-headers --include-follows --verbose
