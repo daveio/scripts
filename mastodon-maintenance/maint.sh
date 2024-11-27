351ef147ca8fce2bbf91f8444c864e237c18175e
Dave Williams <dave@dave.io>
2024-11-27 13:22:48 +0000

    preview cards

--- mastodon-maintenance/maint.sh
+++ mastodon-maintenance/maint.sh
@@ -3,3 +3,4 @@ RAILS_ENV=production bin/tootctl media remove-orphans
 RAILS_ENV=production bin/tootctl media remove --days=7 --verbose
 RAILS_ENV=production bin/tootctl media remove --days=7 --prune-profiles --include-follows --verbose
 RAILS_ENV=production bin/tootctl media remove --days=7 --remove-headers --include-follows --verbose
+RAILS_ENV=production bin/tootctl preview_cards remove --days=7
