9fddf526ed3dd907645911a7cd1536b3a2263bc2
Dave Williams <dave@dave.io>
2024-12-25 14:45:44 +0000

    get sr and bd

--- music/get-audiofile-data/main.py
+++ music/get-audiofile-data/main.py
@@ -1,16 +1,31 @@
 import os
 from mutagen.flac import FLAC
+from mutagen.mp3 import MP3
+from mutagen.mp4 import MP4
+
+decoders = {
+    "flac": FLAC,
+    "mp3": MP3,
+    "m4a": MP4
+}
 
 
-def get_flac_details(directory):
+def get_samplerate_bitdepth(directory):
     for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
-            if filename.endswith('.flac'):
-                file_path = os.path.join(dirpath, filename)
-                audio = FLAC(file_path)
-                print(f'{file_path} : {audio.info.sample_rate}Hz : {audio.info.bits_per_sample}-bit ')
+            ext = os.path.splitext(filename)[1][1:]
+            file_path = os.path.join(dirpath, filename)
+            if ext not in decoders:
+                print(f'{file_path} : NO DECODER)')
+            else:
+                decoder = decoders.get(ext)
+                audio = decoder(file_path)
+                if ext == "mp3":
+                    print(f'{file_path} : {audio.info.sample_rate}Hz')
+                else:
+                    print(f'{file_path} : {audio.info.sample_rate}Hz : {audio.info.bits_per_sample}-bit')
 
 
 # Change the directory path to the top-level directory to start searching
 directory_path = '/Users/dave/Music/out.picard'
-get_flac_details(directory_path)
+get_samplerate_bitdepth(directory_path)
