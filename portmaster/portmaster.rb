e2772956711eb6305a4f017fc71c1c8799310d64
Dave Williams <dave@dave.io>
2025-01-27 22:32:30 +0000

    add ports.json

--- portmaster/portmaster.rb
+++ portmaster/portmaster.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
 require 'json'
+
+begin
+  data = JSON.parse(File.read('ports.json'), symbolize_names: true)
+  items = data.select { |item| item[:rtr] == false }
+  puts items.to_json
+rescue Errno::ENOENT
+  error = { error: 'File not found' }
+  puts JSON.generate(error)
+rescue JSON::ParserError
+  error = { error: 'Invalid JSON format' }
+  puts JSON.generate(error)
+rescue StandardError => e
+  error = { error: e.message }
+  puts JSON.generate(error)
+end
