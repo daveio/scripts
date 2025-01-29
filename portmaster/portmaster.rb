249c895ab0c28cd987a8d72f61c4be90a1fc7ee1
Dave Williams <dave@dave.io>
2025-01-29 03:09:19 +0000

    update portmaster

--- portmaster/portmaster.rb
+++ portmaster/portmaster.rb
@@ -8,7 +8,7 @@ begin
   runnable = ready_to_run.filter do |item|
     item[:attr][:arch].include?('aarch64') || item[:attr][:arch].include?('armhf')
   end
-  runnable.map { |item| item[:source][:url] }.sort.each { |url| puts url unless url.nil? || url.empty? }
+  runnable.map { |item| item[:source][:url] }.sort.uniq.each { |url| puts url unless url.nil? || url.empty? }
 rescue Errno::ENOENT
   error = { error: 'File not found' }
   puts JSON.generate(error)
